17f21458d4bc805c02e48a029b175330bcbaea35
carla <carlajilo@gmail.com>
2022-09-14 19:44:57 +0200

    ejercicio

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let num1 = parseInt(prompt("Introduce un numero"));
+let num2 = parseInt(prompt("Introduce otro numero"));
+
+//ventana emergente
+alert(num1 + num2);
+
+//por consola
+//console.log(num1 + num2);
+
+//por pantalla
+//document.write(num1 + num2);
